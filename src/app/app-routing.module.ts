3d3cd11879faff01278b83dae9b4c514a69163e1
Jonas Marcelo <marcelo.jonas@yahoo.com.br>
2021-12-04 17:28:25 -0300

    Implementando o html e css do LIVRO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { CategoriaDeleteComponent } from './components/views/categoria/categoria
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 import { HomeComponent } from './components/views/home/home.component';
+import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'categorias', component: CategoriaReadComponent},
-  {path: 'categorias/delete/:id', component: CategoriaDeleteComponent},
-  {path: 'categorias/create', component: CategoriaCreateComponent},
-  {path: 'categorias/update/:id', component: CategoriaUpdateComponent},
+  { path: '', component: HomeComponent },
+  { path: 'categorias', component: CategoriaReadComponent },
+  { path: 'categorias/delete/:id', component: CategoriaDeleteComponent },
+  { path: 'categorias/create', component: CategoriaCreateComponent },
+  { path: 'categorias/update/:id', component: CategoriaUpdateComponent },
+  { path:'categorias/:id/livros', component: LivroReadAllComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
+import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { CategoriaUpdateComponent } from './components/views/categoria/categoria
     CategoriaDeleteComponent,
     CategoriaCreateComponent,
     CategoriaUpdateComponent,
+    LivroReadAllComponent,
   ],
   imports: [
     BrowserModule,
